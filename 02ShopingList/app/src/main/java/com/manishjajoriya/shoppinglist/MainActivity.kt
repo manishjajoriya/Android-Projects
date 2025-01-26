2ad0818a6fba109c162fe56bd9763c84d633f1b8
manish jajoriya <manishjajoriya2005@gmail.com>
2025-01-26 19:10:05 +0530

    Project 2: Shopping List

---
+++ 02ShopingList/app/src/main/java/com/manishjajoriya/shoppinglist/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.manishjajoriya.shoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.manishjajoriya.shoppinglist.ui.theme.ShoppingListTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ShoppingListApp(innerPadding)
+                }
+            }
+        }
+    }
+}
+
